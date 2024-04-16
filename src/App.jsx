cec7c3c6e7c719109c2516680eb868afbaca46ab
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-04-16 20:50:56 +0300

    added a footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/UI/Header/Header'
 import Leads from './pages/Leads/Leads'
+import Footer from './components/UI/Footer/Footer'
 
 function App() {
   return (
     <>
       <Header />
       <Leads />
+      <Footer/>
     </>
   )
 }
