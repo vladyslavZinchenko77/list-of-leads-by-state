1b16639441c3891e9ee90cbb9036acef98ab7c0c
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-04-16 19:06:04 +0300

    made a header

---
+++ src/components/UI/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import Card from '@mui/material/Card'
+import HomeIcon from '@mui/icons-material/Home';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+
+import "./Header.scss"
+
+
+const Header = () => {
+  return (
+
+      <header className="header">
+        <nav className='header__nav'>
+          <a className='header__nav-link' href="#"><HomeIcon className='header__nav-link--icon'/>Home</a>
+          <a className='header__nav-link' href="#"><PersonAddIcon className='header__nav-link--icon'/>Leads</a>
+        </nav>
+      </header>
+
+  )
+}
+
+export default Header
